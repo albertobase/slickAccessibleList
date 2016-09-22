32105df269d583023ff00fa154c4dcf2e269dfd4
Alberto González <alberto91base@gmail.com>
2016-09-22 10:02:43 +0200

    Slick modificado para poder recibir una lista y que cumpla accesibilidad.

---
+++ app/app.js
@@ -0,0 +1,127 @@
+'use strict';
+var app = {};
+jQuery( document ).ready(function(){
+    app.main = (function () {
+        var load = function(){
+            init();
+        },
+        init = function(){
+            var target = '.carousel_slick';
+            var $item = $(target);
+            if ($item.length > 0){
+                carousel();
+            }
+        },
+
+        carousel = function(){
+            var $flight_pagination = jQuery('.carousel_slick');
+
+            $flight_pagination.each(function(){
+                var $this = $(this),
+                    classElement = $this.attr('class');
+
+                $flight_pagination.wrap('<div class="' + classElement + '"/>').contents().unwrap();
+            });
+
+            var $flight_pagination = jQuery('.carousel_slick');
+
+            $flight_pagination.each(function(){
+                var $this = jQuery(this);
+
+                $this.slick({
+                    slide: 'li',
+                    dots: false,
+                    infinite: false,
+                    arrows: true,
+                    speed: 300,
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    swipeToSlide: true,
+                    //variableWidth: true,
+                    responsive: [
+                        {
+                            breakpoint: 10000,
+                            settings: {
+                                slidesToShow: 5,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 1239,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 1000,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 767,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
+            });
+        },
+
+        carousel2 = function(){
+            var $flight_pagination = jQuery('.carousel_slick');
+            $flight_pagination.slick({
+                slide: 'li',
+                dots: false,
+                infinite: false,
+                arrows: true,
+                speed: 300,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                swipeToSlide: true,
+                //variableWidth: true,
+                responsive: [
+                    {
+                        breakpoint: 10000,
+                        settings: {
+                            slidesToShow: 5,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 1239,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 1000,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+        };
+      
+        // Public API
+        return {
+            load: load
+        };
+    }());
+
+    app.main.load();
+});
